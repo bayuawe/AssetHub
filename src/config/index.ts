dfbc54588d003a966f5380eed44fdda13b2912d4
bayuawe <bayuaryandi21@gmail.com>
2024-01-06 18:57:40 +0700

    added nav item

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,37 @@ export const PRODUCT_CATEGORIES = [
         href: "#",
         imageSrc: "/nav/ui-kits/mixed.jpg",
       },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Best Seller",
+        href: "#",
+        imageSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorit Icon Picks",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Best Selling Icons",
+        href: "#",
+        imageSrc: "/nav/icons/bestseller.jpg",
+      },
     ],
   },
 ];
