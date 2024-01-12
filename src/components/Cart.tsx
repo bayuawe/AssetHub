37d2d87fbd36e02f443fef2897fa3dfe0308495b
bayuawe <bayuaryandi21@gmail.com>
2024-01-12 22:40:21 +0700

    creating product detail page and persistent shopping cart logic

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,34 +1,49 @@
 "use client";
 
-import { format } from "path";
-import { Separator } from "./ui/separator";
-import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { ShoppingCart } from "lucide-react";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Separator } from "./ui/separator";
 import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import Image from "next/image";
+import { useCart } from "@/hooks/use-cart";
+import { ScrollArea } from "./ui/scroll-area";
+import CartItem from "./CartItem";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
-  const itemCount = 0;
+  const { items } = useCart();
+  const itemCount = items.length;
 
-  const fee = 18000;
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const cartTotal = items.reduce((total, { product }) => total + product.price, 0);
+
+  const fee = 1;
 
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
         <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
-        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-500">0</span>
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{isMounted ? itemCount : 0}</span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle> Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
             <div className="flex w-full flex-col pr-6">
-              {/* TODO: cart logic */}
-              Cart Item
+              <ScrollArea>
+                {items.map(({ product }) => (
+                  <CartItem product={product} key={product.id} />
+                ))}
+              </ScrollArea>
             </div>
             <div className="space-y-4 pr-6">
               <Separator />
@@ -43,9 +58,10 @@ const Cart = () => {
                 </div>
                 <div className="flex">
                   <span className="flex-1">Total</span>
-                  <span>{formatPrice(fee)}</span>
+                  <span>{formatPrice(cartTotal + fee)}</span>
                 </div>
               </div>
+
               <SheetFooter>
                 <SheetTrigger asChild>
                   <Link
@@ -63,9 +79,9 @@ const Cart = () => {
         ) : (
           <div className="flex h-full flex-col items-center justify-center space-y-1">
             <div aria-hidden="true" className="relative mb-4 h-60 w-60 text-muted-foreground">
-              <Image src="/hippo-empty-cart.png" fill alt="empty shopping cart" />
+              <Image src="/hippo-empty-cart.png" fill alt="empty shopping cart hippo" />
             </div>
-            <div className="text-xl font-semibold">Keranjang Kamu masih kosong nih..</div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
             <SheetTrigger asChild>
               <Link
                 href="/products"
@@ -75,7 +91,7 @@ const Cart = () => {
                   className: "text-sm text-muted-foreground",
                 })}
               >
-                Tambahkan item ke keranjang untuk melanjutkan Checkout
+                Add items to your cart to checkout
               </Link>
             </SheetTrigger>
           </div>
